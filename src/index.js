bd5fc0e7da87daa3d93e13c860ea2a7cd3d4361c
yondu <yongdu.lhb@alibaba-inc.com>
2019-08-22 15:46:57 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const wei = require('wei');
+const proxy = require('http-proxy-middleware');
+
+const app = express();
+const appid = '';
+const appsecret = '';
+
+const proxyOption = {
+  target: 'http://www.jarton.cn',
+  pathRewrite: {
+    '^/api': '/api', // 重写请求，api/解析为/
+  },
+  changeOrigoin: true,
+};
+
+app.get('/MP_verify_Mti1B4FBENHQdphV.txt', async (req, res) => {
+  res.send('Mti1B4FBENHQdphV');
+});
+
+app.get('/', async (req, res) => {
+  const { code } = req.query;
+  if (!code) {
+    res.sendFile(`${__dirname}/index.html`);
+    return;
+  }
+  const userInfo = await wei.authorize({
+    appid,
+    appsecret,
+    code, // 此处code可在授权后重定向的url上拿到。
+  }, {
+    check(actoke) {
+      // 这里可以写数据库逻辑
+      // 在这里访问数据库获取，userInfo 就不会去微信拿了
+    },
+  });
+  res.cookie('userId', userInfo);
+  res.setHeader('Content-Type', 'text/html');
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+// 跨域配置
+app.use('/api/*', proxy(proxyOption));
+
+app.listen(80, () => {
+  console.log('Example app listening on port 80!');
+});
